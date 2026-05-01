51bbdf3493d356be5e5e102099abd923d02a7484
Turan Dunyamali <109362422+TuranDunyamali@users.noreply.github.com>
2026-05-01 08:50:35 +0400

    Add files via upload

---
+++ lab12/task7/ThreadBug.java
@@ -0,0 +1,14 @@
+package task7;
+
+public class ThreadBug {
+    public static void main(String[] args) {
+
+        Thread t = new Thread(() -> {
+            System.out.println("Running in another thread");
+        });
+
+        t.start();
+
+        System.out.println("Main finished");
+    }
+}
